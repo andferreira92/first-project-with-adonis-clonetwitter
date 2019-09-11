3035d667ac578d8fb1639e2a0df1da4d431df616
Andson ferreira <andferreira92@outlook.com>
2019-09-11 08:54:01 -0300

    First-Commit

---
+++ start/routes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('/register', 'AuthController.register');
+Route.post('/authenticate', 'AuthController.authenticate');
+
+
+Route.group(() => {
+  Route.resource('tweets', 'TweetController').apiOnly().except('update');
+}).middleware('auth');
